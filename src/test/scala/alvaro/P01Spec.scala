3f45ff096999b199bc87b790ab83974bcdc7ec98
alvarovilaplana <alvaro.vilaplana@gmail.com>
2015-02-17 12:55:44 +0000

    Alvaro - changing the name of the test to match Fran's one

---
+++ src/test/scala/alvaro/P01Spec.scala
@@ -0,0 +1,27 @@
+package alvaro
+
+import org.scalatest.{Matchers, FlatSpec}
+
+import scala.annotation.tailrec
+
+object Finder {
+  @tailrec
+  def last(list: List[Int]): Option[Int] = {
+      list match {
+        case Nil => None
+        case head :: tail if tail == Nil => Some(head)
+        case head :: tail => last(tail)
+      }
+  }
+}
+
+class P01Spec extends FlatSpec with Matchers {
+
+  import Finder._
+
+  "last" should "return the last element in the list" in {
+    val list = List(1, 1, 2, 3, 5, 8)
+    last(list) shouldBe Some(8)
+    last(Nil) shouldBe None
+  }
+}
