5cd492bd2d98a42f5934e631172515d0d6268aab
Haroon Rasheed <Haroon Rasheed>
2015-02-20 15:45:34 +0000

    Completed first excercise and removed unused file

---
+++ src/test/scala/haroon/P01Spec.scala
@@ -0,0 +1,31 @@
+package haroon
+
+import haroon.P01._
+import org.scalatest.{WordSpec, Matchers, FlatSpec}
+
+class P01Spec extends WordSpec with Matchers {
+
+  "P01Spec" should {
+
+    "return Nil on empty list" in {
+      last(List())
+    }
+
+    "return nil on nil list" in {
+      last(Nil)
+    }
+
+    "return last element for a list with one element" in {
+      assert(last(List(2)) == 2)
+
+    }
+
+    "return last element for a list with more than one elements of same type" in {
+      assert(last(List(1, 1, 2, 3, 5, 8)) == 8)
+    }
+
+    "return last element for a list with more than one elements of different types" in {
+      assert(last(List("bar",3,4,5,"foo")) == "foo")
+    }
+  }
+}
