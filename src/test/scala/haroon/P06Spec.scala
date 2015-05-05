abc4bae47de8f5b35363995ff21ce0544263daef
Haroon Rasheed <Haroon Rasheed>
2015-05-05 21:29:15 +0100

    added more tests to P06

--- src/test/scala/haroon/P06Spec.scala
+++ src/test/scala/haroon/P06Spec.scala
@@ -21,6 +21,30 @@ class P06Spec extends WordSpec with Matchers {
       P06.isPalindrome(List(1, 2, 3, 2, 1)) shouldBe true
     }
 
+    "return true when the list is (1)" in {
+      P06.isPalindrome(List(1)) shouldBe true
+    }
+
+    "return true when the list is (1,1)" in {
+      P06.isPalindrome(List(1, 1)) shouldBe true
+    }
+
+    "return true wen the list is (1, 2, 1) " in {
+      P06.isPalindrome(List(1, 2, 1)) shouldBe true
+    }
+
+    "return true wen the list is (1, 2, 2, 1) " in {
+      P06.isPalindrome(List(1, 2, 2, 1)) shouldBe true
+    }
+
+    "return true when the list is (1, 2, 3, 2, 1) " in {
+      P06.isPalindrome(List(1, 2, 3, 2, 1)) shouldBe true
+    }
+
+    "return false when the list is (1,2,3)" in {
+      P06.isPalindrome(List(1, 2, 3)) shouldBe false
+    }
+
     "return false for a non-palindrome list" in {
       P06.isPalindrome(List(2, 2, 3, 2, 1)) shouldBe false
     }
