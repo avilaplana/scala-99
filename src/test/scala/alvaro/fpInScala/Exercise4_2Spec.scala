9941862ab958e72e24a808f5e19025b214eacef0
alvarovilaplanagarcia <alvaro.vilaplana@gmail.com>
2015-05-24 21:28:31 +0100

    Alvaro - 4.2

---
+++ src/test/scala/alvaro/fpInScala/Exercise4_2Spec.scala
@@ -0,0 +1,34 @@
+package alvaro.fpInScala
+
+import alvaro.fpInScala.Option
+import org.scalatest.{Matchers, WordSpecLike}
+
+//EXERCISE 2:
+//Implement the variance function (if the mean is m, variance is the mean of math.pow(x - m, 2), see definition) in terms of mean and
+//flatMap.
+//Variance can actually be computed in one pass, but for pedagogical purposes we will compute it using two passes.
+//The first will compute the mean of the data set, and the second will compute the mean squared difference from this mean.
+class Exercise4_2Spec extends WordSpecLike with Matchers {
+
+  def variance(xs: Seq[Double]): Option[Double] = {
+    def mean(xs: Seq[Double]): Option[Double] = {
+      xs match {
+        case Nil => None
+        case seq => Some(seq.sum / seq.size)
+      }
+    }
+
+    mean(xs).flatMap(m => mean(xs.map(e => math.pow(e - m, 2))))
+  }
+
+
+  "variance" should {
+    "return 0.66666666666666666 for the list 1,2,3" in {
+      variance(Seq(1, 2, 3)) shouldBe Some(0.66666666666666666)
+    }
+
+    "return 2.91666666666666666 for the list 1,2,3,4,5,6" in {
+      variance(Seq(1, 2, 3, 4, 5, 6)) shouldBe Some(2.91666666666666666)
+    }
+  }
+}
